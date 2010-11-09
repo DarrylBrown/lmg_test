328d938ad59c76990fee9b5b5977d11feba8c822
admin-darryl brown <darryl.brown@emerson.com>
2010-11-09 08:59:43 -0500

    Clean up comments section.
    Correct the file name used in the command line example
    
    Remove comment regarding need to make additional changes
    for the spaces at end of file

--- Tools/chng_testsite.rb
+++ Tools/chng_testsite.rb
@@ -1,21 +1,19 @@
 =begin
 Change the test_site in the windows host file
 The new ip is passed as a command line argument
-  example: chng-test_site.rb 1.2.3.4
+  example: chng_testsite.rb 1.2.3.4
 
 A regex matches the line that contains an 'ip' AND 'test-site'
 The file stream is opened and read the match is found and replaced.
 The stream is then rewound (rewind) and written (puts) back to file. 
 
-Reason for the puts '    ' 
+Reason for the f.puts'    '
 If the new ip char length is less than previous 
   i.e. 1.2.3.4 -vs- 10.20.30.40
   then the last char in file are re-written
   these spaces will overwrite any added chars.
-  May need to find a beeter way to handle this as the 
-  as the file will progessively get longer each time this script is ran.
-  
-The file is automatically saved.
+   
+The hosts file is automatically saved.
 =end
 
 
